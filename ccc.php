631f571ec6da8eb2eb9d6b585ae8904e8dae6fd1
garnierg <germain.garnier@altares.com>
2017-07-10 14:03:01 +0200

    POST + error format

--- ccc.php
+++ ccc.php
@@ -6,30 +6,32 @@ $s = $_GET['Company'];
 $sP = $_POST['Company'];
 
 if ($sP!=null && $key !=null){
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://search.altares.fr/search?searchChunk=".$sP,
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "GET",
-  CURLOPT_HTTPHEADER => array(
-    "x-api-key: ".$key
-  ),
-));
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://search.altares.fr/search?searchChunk=".$sP,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => array(
+      "x-api-key: ".$key
+    ),
+  ));
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
 
-curl_close($curl);
-header('Content-Type: application/json');
+  curl_close($curl);
+  header('Content-Type: application/json');
 
   if ($err) {
     //echo "cURL Error #:" . $err;
-    echo '"status": {
+    echo '{
+      "status": {
       "code": 500,
       "errorType": "ERR-'.$err.'"
+      }
     }';
   } else {
 
@@ -45,9 +47,11 @@ header('Content-Type: application/json');
 
   }
 } else {
-  echo '"status": {
+  echo '{
+    "status": {
     "code": 400,
     "errorType": "Empty request"
+    }
   }';
   //echo "Empty request";
 }
