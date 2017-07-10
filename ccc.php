28b256e58bd64c441f371e92f57f2698a6a19a85
garnierg <germain.garnier@altares.com>
2017-07-10 15:26:05 +0200

    data

--- ccc.php
+++ ccc.php
@@ -47,11 +47,14 @@ if ($sP!=null && $key !=null){
     */
   } else {
 
+    $json = json_decode($response);
+    $data = array_map('convert', $json);
     //echo $response;
     //echo json_decode($response);
      echo '{
           "speech": "Voici les informations concernant '.$sP.'",
           "displayText": "Voici les informations concernant '.explode(", ",$response).'",
+          "data":"'.$data.'",
           "source": "apiai-dirigeant-company-altares"
       }';
 
@@ -70,4 +73,36 @@ if ($sP!=null && $key !=null){
        "source": "apiai-dirigeant-company-altares"
    }';
 }
+
+// HELPERS
+
+function convert($item) {
+  $res = array();
+  $res['value'] = $item->legalName;
+  $res['name'] = $item->legalName;
+  $res['siren'] = $item->siren;
+  $res['activity'] = $item->nafDescription;
+  $res['venue'] = $item->address->rnvpL4.', '.$item->address->rnvpL6;
+  $res['url'] = '--'.$item->siren.'?utm_source=altares.fr&utm_medium=referral&utm_campaign=searchhome';
+  return $res;
+}
+
+/*
+function as_get_search($request) {
+  $data = [];
+
+  if ( isset( $request['term'] ) ) {
+    $term = $request['term'];
+    if( strlen( $term ) > 1 ) {
+      $opts = array('http' => array('header' => array('x-api-key: uwNPsInIeo59yK34sbnjB5R5dtsQzZSz9Jt7OZeN')));
+      $context = stream_context_create($opts);
+      $response = file_get_contents('https://search.altares.fr/search?searchChunk=' . $term . '&isActiv=true', false, $context);
+      $json = json_decode($response);
+      $data = array_map('convert', $json);
+    }
+  }
+
+    return rest_ensure_response( $data );
+  }
+  */
 ?>
