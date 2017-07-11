b939f486dde839504e8cc4a2710c25e85e3cf2c7
garnierg <germain.garnier@altares.com>
2017-07-11 17:22:01 +0200

    Add dirigeant action

--- ccc.php
+++ ccc.php
@@ -1,6 +1,5 @@
 <?php
 
-$curl = curl_init();
 $key = $_GET['key'];
 
 // GET DATA FROM API.AI CALL
@@ -14,14 +13,35 @@ $sP = $parameters['Company'];
 if($sP == ""){
   $sP = $_GET['Company'];
 }
+// get action (testing purposes) by get if not via api.ai
+if($action == ""){
+  $action = $_GET['action'];
+}
 
-if ($sP!=null && $key !=null){
+if ($sP!=null && $key !=null && $action!=null){
   //remove spaces in siren
   if (is_numeric(str_replace(" ","", $sP))) {
     $sP = str_replace(" ","", $sP);
   }
 
+  if($action=="search") {
+    search($sP);
+  } else if($action=="dirigeant"){
+    findDirigeant($sP);
+  }
+  } else {
+    echo '{
+         "speech": "Pas de données utilisables '.$sP.'",
+         "displayText": "Pas de données utilisables '.$sP.'",
+         "source": "apiai-dirigeant-company-altares-'.$action.'"
+     }';
+ }
+
+// HELPERS
+function search($sP) {
+  GLOBAL $key,$action;
   // CALL TO OUR LOCAL WS tO GET COMPANY DATA
+  $curl = curl_init();
   curl_setopt_array($curl, array(
     CURLOPT_URL => "https://search.altares.fr/search?searchChunk=".urlencode($sP)."&isActiv=true",
     CURLOPT_RETURNTRANSFER => true,
@@ -37,10 +57,12 @@ if ($sP!=null && $key !=null){
 
   $response = curl_exec($curl);
   $err = curl_error($curl);
-
   curl_close($curl);
 
+
+  // SET response Header type to JSON
   header('Content-Type: application/json');
+
   if ($err || $response==null) {
     echo '{
          "speech": "err '.$err.'",
@@ -51,6 +73,8 @@ if ($sP!=null && $key !=null){
     $json = json_decode($response);
     if(count($json)>0){
       if(count($json)==1){
+        print_r($json);
+
         $data = array_map('convert', $json);
         $prez = $data[0]['value']." (".$data[0]['siren'].") \n\r A l'adresse suivante : ".$data[0]['venue']."\n\r".$data[0]['activity']."";
         echo '{
@@ -81,21 +105,77 @@ if ($sP!=null && $key !=null){
        }';
     }
   }
-} else {
-/*  echo '{
-    "status": {
-    "code": 400,
-    "errorType": "Empty request"
-    }
-  }';*/
-  echo '{
-       "speech": "Pas de données utilisables '.$sP.'",
-       "displayText": "Pas de données utilisables '.$sP.'",
-       "source": "apiai-dirigeant-company-altares"
-   }';
 }
 
-// HELPERS
+function findDirigeant($sP) {
+  GLOBAL $key,$action;
+  // CALL TO OUR LOCAL WS tO GET COMPANY DATA
+  $curl = curl_init();
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://search.altares.fr/identity/".urlencode($sP),
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => array(
+      "x-api-key: ".$key
+    ),
+  ));
+
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
+  curl_close($curl);
+
+
+  // SET response Header type to JSON
+  header('Content-Type: application/json');
+
+  if ($err || $response==null) {
+    echo '{
+         "speech": "err '.$err.'",
+         "displayText": "err '.$err.'",
+         "source": "apiai-dirigeant-company-altares-'.$action.'"
+     }';
+  } else {
+    $json = json_decode($response);
+    if(count($json)>0){
+      if(count($json)==1){
+        //print_r($json);
+        //$data = array_map('convertD', $json);
+        $prez = "Le dirigeant de la société ".$json->value." (".$json->siren.")
+        est ".$json->director->firstName." ".$json->director->lastName." en tant que ".$json->director->label. " depuis ".$json->director->startAt;
+        echo '{
+              "speech": "Voici : '.$prez.'",
+              "displayText": "Voici : '.$prez.'",
+              "data":'.json_encode($json).',
+              "source": "apiai-dirigeant-company-altares-'.$action.'"
+            }';
+      } else {
+        $data = array_map('convertD', $json);
+        $prez="J'ai trouvé ".count($data)." entreprises avec les informations que vous m'avez donné...\n\r
+                Laquelle vous intéresse ?\r\n";
+        for($i=0;$i<count($data);$i++){
+          $prez.= $data[$i]['value']." (".$data[$i]['siren'].") \n\r A l'adresse suivante : ".$data[$i]['rnvpL6'].". \n\r";
+        }
+        echo '{
+              "speech": "'.$prez.'",
+              "displayText": "'.$prez.'",
+              "data":'.json_encode($data[0]).',
+              "source": "apiai-dirigeant-company-altares-'.$action.'"
+            }';
+      }
+    }else{
+      echo '{
+           "speech": "Pas de résultats utilisables '.$sP.'",
+           "displayText": "Pas de résultats utilisables '.$sP.'",
+           "source": "apiai-dirigeant-company-altares-'.$action.'"
+       }';
+    }
+  }
+}
+
 
 function convert($item) {
   $res = array();
@@ -110,22 +190,6 @@ function convert($item) {
   return $res;
 }
 
-/*
-function as_get_search($request) {
-  $data = [];
 
-  if ( isset( $request['term'] ) ) {
-    $term = $request['term'];
-    if( strlen( $term ) > 1 ) {
-      $opts = array('http' => array('header' => array('x-api-key: uwNPsInIeo59yK34sbnjB5R5dtsQzZSz9Jt7OZeN')));
-      $context = stream_context_create($opts);
-      $response = file_get_contents('https://search.altares.fr/search?searchChunk=' . $term . '&isActiv=true', false, $context);
-      $json = json_decode($response);
-      $data = array_map('convert', $json);
-    }
-  }
 
-    return rest_ensure_response( $data );
-  }
-  */
 ?>
