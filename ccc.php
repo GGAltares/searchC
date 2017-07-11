6ae5e6c7b8aa318f1e98e1734ededa584dc22c51
garnierg <germain.garnier@altares.com>
2017-07-11 09:43:10 +0200

    multiple type of response

--- ccc.php
+++ ccc.php
@@ -57,15 +57,29 @@ if ($sP!=null && $key !=null){
   } else {
     $json = json_decode($response);
     if(count($json)>0){
-    $data = array_map('convert', $json);
-    $prez = $data[0]['value']." (".$data[0]['siren'].") \n\r A l'adresse suivante : ".$data[0]['venue']."\n\r".$data[0]['activity']."";
-    echo '{
-          "speech": "Voici les informations concernant '.$prez.'",
-          "displayText": "Voici les informations concernant '.$prez.'",
-          "data":'.json_encode($data[0]).',
-          "source": "apiai-dirigeant-company-altares"
-      }';
-
+      if(count($json)==1){
+        $data = array_map('convert', $json);
+        $prez = $data[0]['value']." (".$data[0]['siren'].") \n\r A l'adresse suivante : ".$data[0]['venue']."\n\r".$data[0]['activity']."";
+        echo '{
+              "speech": "Voici les informations concernant '.$prez.'",
+              "displayText": "Voici les informations concernant '.$prez.'",
+              "data":'.json_encode($data[0]).',
+              "source": "apiai-dirigeant-company-altares"
+            }';
+      } else {
+        $data = array_map('convert', $json);
+        $prez="J'ai trouvé ".count($data)." entreprises avec les informations que vous m'avez donné...\n\r
+                Laquelle vous intéresse ?\r\n";
+        for($i=0;$i<count($data);$i++){
+          $prez.= $data[$i]['value']." (".$data[$i]['siren'].") \n\r A l'adresse suivante : ".$data[$i]['rnvpL6'].". \n\r";
+        }
+        echo '{
+              "speech": "'.$prez.'",
+              "displayText": "'.$prez.'",
+              "data":'.json_encode($data[0]).',
+              "source": "apiai-dirigeant-company-altares"
+            }';
+      }
     }else{
       echo $sP . "   ".$json;
       echo '{
@@ -97,6 +111,8 @@ function convert($item) {
   $res['name'] = $item->legalName;
   $res['siren'] = $item->siren;
   $res['activity'] = $item->nafDescription;
+  $res['rnvpL4'] = $item->address->rnvpL4;
+  $res['rnvpL6'] = $item->address->rnvpL6;
   $res['venue'] = $item->address->rnvpL4.', '.$item->address->rnvpL6;
   $res['url'] = '--'.$item->siren.'?utm_source=altares.fr&utm_medium=referral&utm_campaign=searchhome';
   return $res;
